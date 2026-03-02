01646ec12bbf5015360d8956b0267e293582fc95
Wesley <wesleysimpson02@gmail.com>
2026-03-02 14:44:47 -0500

    Fix Counter and RevealAnswer arrow function ESLint issues

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -3,9 +3,16 @@ import { Button } from "react-bootstrap";
 
 export function Counter(): React.JSX.Element {
     const [value, setValue] = useState<number>(0);
+
     return (
         <span>
-            <Button onClick={() => setValue(1 + value)}>Add One</Button>
+            <Button
+                onClick={() => {
+                    setValue(value + 1);
+                }}
+            >
+                Add One
+            </Button>{" "}
             to {value}.
         </span>
     );
